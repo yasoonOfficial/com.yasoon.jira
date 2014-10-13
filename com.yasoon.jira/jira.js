3dac978b7c78c1ec0b3fd1e2f1ac3ee7c20a75f2
tobiasviehweger <tobias.viehweger@gmx.de>
2014-10-13 19:11:36 +0200

    module file

---
+++ com.yasoon.jira/jira.js
@@ -0,0 +1,7 @@
+yasoon.app.load("com.yasoon.jira", new function () { //jshint: ignore:line
+    this.init = function () {
+        console.log("jira starting..");
+    };
+});
+
+//@ sourceURL=jira.js
