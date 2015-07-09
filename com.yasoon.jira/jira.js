9fb64f47b3d0d632b864bccbc07b669c3dccac73
DarkLegend <andy.feudenheim@gmail.com>
2015-07-09 11:48:22 +0200

    Fix ActivityLog

--- com.yasoon.jira/jira.js
+++ com.yasoon.jira/jira.js
@@ -14,7 +14,8 @@ yasoon.app.load("com.yasoon.jira", new function () { //jshint ignore:line
 	jira.firstSyncedNotifications = 0;
 	var startSync = new Date();
 	var currentPage = 1;
- 
+	var oAuthSuccess = false;
+
 	this.lifecycle = function(action, oldVersion, newVersion) {
 		if (action === yasoon.lifecycle.Upgrade) {
 			if (newVersion == "0.6.1") {
@@ -93,7 +94,7 @@ yasoon.app.load("com.yasoon.jira", new function () { //jshint ignore:line
 			}
 		]);
 		yasoon.on("sync", jira.sync);
-		yasoon.app.on("oAuthSuccess", jira.sync);
+		yasoon.app.on("oAuthSuccess", jira.handleOAuthSuccess);
 		yasoon.periodicCallback(300, jira.sync);
 	};
 
@@ -107,153 +108,158 @@ yasoon.app.load("com.yasoon.jira", new function () { //jshint ignore:line
 	};
 
 	this.syncData = function () {
-	    var currentTs = new Date().getTime();
-	    var oldTs = jira.settings.lastSync.getTime() - 1000;
+		var currentTs = new Date().getTime();
+		var oldTs = jira.settings.lastSync.getTime() - 1000;
 
-	    jiraLog('Sync starts: ' + new Date().toISOString());
-	    return self.initData()
-        .then(function () {
-            jira.issues.refreshBuffer();
-            return self.syncStream('/activity?streams=update-date+BETWEEN+' + oldTs + '+' + currentTs);
-        })
-        .then(function () {
-            return jira.notifications.processChildren();
-        })
-        .then(function () {
-            return jira.notifications.processCommentEdits();
-        })
-        .then(function () {
-            jira.settings.setLastSync(startSync);
-            jiraLog('Sync done: ' + new Date().toISOString());
-			if(jira.firstTime) {
-				yasoon.util.logActivity('syncCompleted', JSON.stringify({
+		jiraLog('Sync starts: ' + new Date().toISOString());
+		return self.initData()
+		.then(function () {
+			jira.issues.refreshBuffer();
+			return self.syncStream('/activity?streams=update-date+BETWEEN+' + oldTs + '+' + currentTs);
+		})
+		.then(function () {
+			return jira.notifications.processChildren();
+		})
+		.then(function () {
+			return jira.notifications.processCommentEdits();
+		})
+		.then(function () {
+			jira.settings.setLastSync(startSync);
+			jiraLog('Sync done: ' + new Date().toISOString());
+
+			if(oAuthSuccess){
+				yasoon.util.logActivity('oAuthSuccess', JSON.stringify({
 					app: 'com.yasoon.jira',
 					count: jira.firstSyncedNotifications
 				}));
 			}
-        })
-        .catch(jiraSyncError, function (error) {
-            jiraLog('Sync Error:', error);
-        })
-        .catch(function (e) {
-            yasoon.util.log(e.message, yasoon.util.severity.error, getStackTrace(e));
-        })
+		})
+		.catch(jiraSyncError, function (error) {
+			jiraLog('Sync Error:', error);
+		})
+		.catch(function (e) {
+			yasoon.util.log(e.message, yasoon.util.severity.error, getStackTrace(e));
+		})
 		.finally(function() {
 			jira.notifications.showDesktopNotif();
 		});
 	};
 
+	this.handleOAuthSuccess = function () {
+		oAuthSuccess = true;
+		self.sync();
+	};
 	//Handle Sync Event
 	this.syncStream = function (url, maxResults, currentPage) {
-	    //Defaults
-	    maxResults = maxResults || jira.CONST_PULL_RESULTS;
-	    currentPage = currentPage || 0;
+		//Defaults
+		maxResults = maxResults || jira.CONST_PULL_RESULTS;
+		currentPage = currentPage || 0;
 
-        //Add maxResults to URL
-	    if (url.indexOf('?') === -1) 
-	        url += '?maxResults=' + maxResults;
-	    else 
+		//Add maxResults to URL
+		if (url.indexOf('?') === -1) 
+			url += '?maxResults=' + maxResults;
+		else 
 			url += '&maxResults=' + maxResults;
 	
 		jiraLog('Get Activity Stream');
 
 		return jiraGet(url)
-        .then(function (xmlData) {
-            var jsonPage = jiraXmlToJson(new DOMParser().parseFromString(xmlData, "text/xml"));
-            jiraLog('page:', jsonPage);
-            if (jsonPage.feed && jsonPage.feed.entry) {
-                //Adjust Data. If it's only 1 entry it's an object instead of array
-                if (!$.isArray(jsonPage.feed.entry)) {
-                    jsonPage.feed.entry = [jsonPage.feed.entry];
-                }
-                return jsonPage.feed.entry;
-            }
-            return [];
-        })
-        .each(function (feedEntry, index) {
-            jiraLog('Item #' + index + ':', feedEntry);
-            //Only for jira!
-            if (feedEntry['atlassian:application'] && feedEntry['atlassian:application']['#text'].toLowerCase().indexOf('jira') > -1) {
-            	var notif = jira.notifications.createNotification(feedEntry);
+		.then(function (xmlData) {
+			var jsonPage = jiraXmlToJson(new DOMParser().parseFromString(xmlData, "text/xml"));
+			jiraLog('page:', jsonPage);
+			if (jsonPage.feed && jsonPage.feed.entry) {
+				//Adjust Data. If it's only 1 entry it's an object instead of array
+				if (!$.isArray(jsonPage.feed.entry)) {
+					jsonPage.feed.entry = [jsonPage.feed.entry];
+				}
+				return jsonPage.feed.entry;
+			}
+			return [];
+		})
+		.each(function (feedEntry, index) {
+			jiraLog('Item #' + index + ':', feedEntry);
+			//Only for jira!
+			if (feedEntry['atlassian:application'] && feedEntry['atlassian:application']['#text'].toLowerCase().indexOf('jira') > -1) {
+				var notif = jira.notifications.createNotification(feedEntry);
 				
 				if(jira.firstTime) 
 					jira.firstSyncedNotifications++;
 				
 				if(notif)
 					return notif.save();
-            }
-        })
-        .then(function (entries) {
-            //Determine if paging is required
-            if (entries && entries.length > 0) {
-                var lastObj = entries[entries.length - 1];
-                var lastObjDate = new Date(lastObj.updated['#text']);
+			}
+		})
+		.then(function (entries) {
+			//Determine if paging is required
+			if (entries && entries.length > 0) {
+				var lastObj = entries[entries.length - 1];
+				var lastObjDate = new Date(lastObj.updated['#text']);
 
-                if (entries.length == maxResults && jira.settings.lastSync < lastObjDate && currentPage < 2) {
-                    return self.syncStream('/activity?streams=update-date+BEFORE+' + (lastObjDate.getTime() - 2000), maxResults, currentPage + 1);
-                }
-            }
-        });
+				if (entries.length == maxResults && jira.settings.lastSync < lastObjDate && currentPage < 2) {
+					return self.syncStream('/activity?streams=update-date+BEFORE+' + (lastObjDate.getTime() - 2000), maxResults, currentPage + 1);
+				}
+			}
+		});
 
 	};
 
 	this.initData = function () {
-        //First Get Own User Data
-	    jiraLog('Get Own Data');
-	    if (jira.firstTime) {
-	        return jiraGet('/rest/api/2/myself')
-            .then(function (ownUserData) {
-                jira.data.ownUser = JSON.parse(ownUserData);
-            })
-            .then(function () {
-                //Second get all projects
-                jiraLog('Get Projects');
-                return jiraGet('/rest/api/2/project')
-                .then(function (projectData) {
-                    var projects = JSON.parse(projectData);
-                    jira.data.projects = [];
-                    return projects;
-                })
-                .each(function (project) {
-                    //Get detailed information for each project
-                    return jiraGet('/rest/api/2/project/' + project.id)
-                    .then(function (singleProject) {
-                        var proj = JSON.parse(singleProject);
-                        jira.data.projects.push(proj);
-                    });
-                });
-            })
-            .then(function () {
-                //Third get all issue types
-                jiraLog('Get Issuetypes');
-                return jiraGet('/rest/api/2/issuetype')
-                .then(function (issueTypes) {
-                    jira.data.issueTypes = JSON.parse(issueTypes);
-                    jira.settings.updateData();
-                    jira.firstTime = false;
-                });
-            });
-	    } else {
-	        return Promise.resolve();
-	    }
+		//First Get Own User Data
+		jiraLog('Get Own Data');
+		if (jira.firstTime) {
+			return jiraGet('/rest/api/2/myself')
+			.then(function (ownUserData) {
+				jira.data.ownUser = JSON.parse(ownUserData);
+			})
+			.then(function () {
+				//Second get all projects
+				jiraLog('Get Projects');
+				return jiraGet('/rest/api/2/project')
+				.then(function (projectData) {
+					var projects = JSON.parse(projectData);
+					jira.data.projects = [];
+					return projects;
+				})
+				.each(function (project) {
+					//Get detailed information for each project
+					return jiraGet('/rest/api/2/project/' + project.id)
+					.then(function (singleProject) {
+						var proj = JSON.parse(singleProject);
+						jira.data.projects.push(proj);
+					});
+				});
+			})
+			.then(function () {
+				//Third get all issue types
+				jiraLog('Get Issuetypes');
+				return jiraGet('/rest/api/2/issuetype')
+				.then(function (issueTypes) {
+					jira.data.issueTypes = JSON.parse(issueTypes);
+					jira.settings.updateData();
+					jira.firstTime = false;
+				});
+			});
+		} else {
+			return Promise.resolve();
+		}
 	};
 
 	this.handleError = function (data, statusCode, result, errorText, cbkParam) {
-	    self.handleErrorSoft(data, statusCode, result, errorText, cbkParam);
-	    jira.SyncInProcess = false;
+		self.handleErrorSoft(data, statusCode, result, errorText, cbkParam);
+		jira.SyncInProcess = false;
 	};
 
 	this.handleErrorSoft = function (data, statusCode, result, errorText, cbkParam) {
-	    console.log(statusCode + ' || ' + errorText + ' || ' + result + ' || ' + data);
+		console.log(statusCode + ' || ' + errorText + ' || ' + result + ' || ' + data);
 
-	    //Detect if oAuth token has become invalid
-	    if (statusCode == 401 && result == 'oauth_problem=token_rejected') {
-	        yasoon.app.invalidateOAuthToken(jira.settings.currentService);
-	    }
+		//Detect if oAuth token has become invalid
+		if (statusCode == 401 && result == 'oauth_problem=token_rejected') {
+			yasoon.app.invalidateOAuthToken(jira.settings.currentService);
+		}
 
-	    if (cbkParam && cbkParam.fail) {
-	        cbkParam.fail();
-	    }
+		if (cbkParam && cbkParam.fail) {
+			cbkParam.fail();
+		}
 	};
 
 }); //jshint ignore:line
