1afe6798954347a2b8d5922723ca0116e974e3b1
Tobias Viehweger <tobias.viehweger@yasoon.com>
2017-02-27 18:16:05 +0100

    fix signing

--- compile_sign.bat
+++ compile_sign.bat
@@ -6,7 +6,7 @@ CMD /C node build.js
 
 ..\yasoonFrontendRedesign\yasoonBase\distTools\7z a com.yasoon.jira-%version%.zip .\distribution\*
 
-openssl dgst -sha256 com.yasoon.jira-%version%.zip > com.yasoon.jira-%version%.zip.hash
+openssl dgst -sha256 -binary com.yasoon.jira-%version%.zip > com.yasoon.jira-%version%.zip.hash
 openssl rsautl -sign -inkey ..\yasoonFrontendRedesign\yasoonBase\yasoonBase\yasoonfixed.pfx -keyform pkcs12 -passin pass:%SSL_PW% -in com.yasoon.jira-%version%.zip.hash > com.yasoon.jira-%version%.zip.sig
 
 del com.yasoon.jira-%version%.zip.hash
